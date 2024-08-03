6bdc1e717c3d63dcf501a56bda539ef6619ba8a6
Vjola Sulaj <sulaj.vjolaa@gmail.com>
2024-08-03 18:40:56 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { HashRouter as Router } from "react-router-dom";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { HashRouter as Router } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    loader:rootLoader,
-  },
-]);
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <App />
   </Router>
-);
+
+)
