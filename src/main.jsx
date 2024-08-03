d1ff9ac12dd76244c7edf384b76ffa9f6dbd5d45
Vjola Sulaj <sulaj.vjolaa@gmail.com>
2024-08-03 18:37:49 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { HashRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { HashRouter as Router } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/gallery",
+    element: <Root />,
+    loader:rootLoader,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <App />
   </Router>
-
-)
+);
