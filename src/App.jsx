5f758ef1ee8a184d5db328c77723a9e646b259fb
Vjola Sulaj <sulaj.vjolaa@gmail.com>
2024-08-03 18:23:17 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
-        <Route path="/administrator" element={<Admin />} />
+        {/* <Route path="/administrator" element={<Admin />} /> */}
       </Routes>
     </div>
   );
